91dc21cb7ddde83928a48eb8a85e33bab9f24229
peswanibros <peswani.sunny121@gmail.com>
2021-01-09 18:50:37 +0530

    aware activity

--- android/src/main/kotlin/com/direct_share/DirectSharePlugin.kt
+++ android/src/main/kotlin/com/direct_share/DirectSharePlugin.kt
@@ -1,20 +1,27 @@
 package com.direct_share
 
+import android.content.pm.PackageManager
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry
 
 /** DirectSharePlugin */
-class DirectSharePlugin : FlutterPlugin, MethodCallHandler {
+class DirectSharePlugin : FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.RequestPermissionsResultListener {
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
     /// when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
     private lateinit var share: Share;
+    private var activityBinding: ActivityPluginBinding? = null
+    private val CODE_ASK_PERMISSION = 100
+
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "direct_share")
         channel.setMethodCallHandler(this)
@@ -34,5 +41,36 @@ class DirectSharePlugin : FlutterPlugin, MethodCallHandler {
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activityBinding = binding
+
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        onDetachedFromActivity()
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        onAttachedToActivity(binding)
+
+    }
+
+    override fun onDetachedFromActivity() {
+        tearDown()
+    }
+
+    private fun tearDown() {
+        activityBinding!!.removeRequestPermissionsResultListener(this)
+        activityBinding = null
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>?, grantResults: IntArray?): Boolean {
+        if (requestCode == CODE_ASK_PERMISSION && grantResults!!.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            share.share()
+        }
+        return false
     }
 }
